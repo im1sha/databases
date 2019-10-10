2f8fb0251c8ef2a85e9736891d71e1d603ab8620
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-10-10 23:09:59 +0300

    lab3 completed

--- lab3/lab3task2.sql
+++ lab3/lab3task2.sql
@@ -7,9 +7,9 @@
 -- вычисляемое поле SalesDiff, 
 -- считающее разницу значений в полях SalesYTD и SalesLastYear.
 ALTER TABLE [dbo].[Person] ADD [SalesYTD] MONEY NULL
-	,[SalesLastYear] MONEY NULL
-	,[OrdersNum] INT NULL
-	,[SalesDiff] AS ([SalesYTD] - [SalesLastYear]);
+	 ,[SalesLastYear] MONEY NULL
+	 ,[OrdersNum] INT NULL
+	 ,[SalesDiff] AS ([SalesYTD] - [SalesLastYear]);
 
 ------------------------------------------------------------------
 -- b) 
@@ -19,27 +19,27 @@ ALTER TABLE [dbo].[Person] ADD [SalesYTD] MONEY NULL
 -- таблицы dbo.Person за исключением поля SalesDiff.
 IF OBJECT_ID('tempdb..#Person') IS NOT NULL
 BEGIN
-	DROP TABLE #Person;
+	 DROP TABLE #Person;
 
-	PRINT 'DROP';
+	 PRINT 'DROP';
 END
 GO
 
 CREATE TABLE #Person (
-	[BusinessEntityID] INT NOT NULL PRIMARY KEY
-	,[PersonType] NVARCHAR(2) NOT NULL
-	,[NameStyle] BIT NOT NULL
-	,[Title] NVARCHAR(8) NULL
-	,[FirstName] NVARCHAR(50) NOT NULL
-	,[MiddleName] NVARCHAR(50) NULL
-	,[LastName] NVARCHAR(50) NOT NULL
-	,[Suffix] NVARCHAR(5) NULL
-	,[EmailPromotion] INT NOT NULL
-	,[ModifiedDate] DATETIME NOT NULL
-	,[SalesYTD] MONEY NULL
-	,[SalesLastYear] MONEY NULL
-	,[OrdersNum] INT NULL
-	);
+	 [BusinessEntityID] INT NOT NULL PRIMARY KEY
+	 ,[PersonType] NVARCHAR(2) NOT NULL
+	 ,[NameStyle] BIT NOT NULL
+	 ,[Title] NVARCHAR(8) NULL
+	 ,[FirstName] NVARCHAR(50) NOT NULL
+	 ,[MiddleName] NVARCHAR(50) NULL
+	 ,[LastName] NVARCHAR(50) NOT NULL
+	 ,[Suffix] NVARCHAR(5) NULL
+	 ,[EmailPromotion] INT NOT NULL
+	 ,[ModifiedDate] DATETIME NOT NULL
+	 ,[SalesYTD] MONEY NULL
+	 ,[SalesLastYear] MONEY NULL
+	 ,[OrdersNum] INT NULL
+	 );
 GO
 
 ------------------------------------------------------------------
@@ -51,51 +51,62 @@ GO
 -- Sales.SalesOrderHeader и заполните этими значениями поле
 -- OrdersNum. Подсчет количества заказов осуществите 
 -- в Common Table Expression (CTE).
-WITH CTE
+WITH CteOrdersNum
 AS (
-	SELECT poh.[EmployeeID]
-		,SUM(poh.[SubTotal]) AS [SumSubTotal]
-	FROM [Purchasing].[PurchaseOrderHeader] poh
-	GROUP BY poh.[EmployeeID]
-	)
+	 SELECT COUNT(*) AS [OrdersNum]
+	 	 ,[SalesPersonID]
+	 FROM [Sales].[SalesOrderHeader] soh
+	 GROUP BY soh.[SalesPersonID]
+	 )
 INSERT INTO #Person (
-	[BusinessEntityID]
-	,[PersonType]
-	,[NameStyle]
-	,[Title]
-	,[FirstName]
-	,[MiddleName]
-	,[LastName]
-	,[Suffix]
-	,[EmailPromotion]
-	,[ModifiedDate]
-	,[SalesYTD]
-	,[SalesLastYear]
-	,[OrdersNum]
-	)
-SELECT [person].[BusinessEntityID]
-	,[person].[PersonType]
-	,[person].[NameStyle]
-	,[person].[Title]
-	,[person].[FirstName]
-	,[person].[MiddleName]
-	,[person].[LastName]
-	,[person].[Suffix]
-	,[person].[EmailPromotion]
-	,[person].[ModifiedDate]
-	,[sales].[SalesYTD]
-	,[sales].[SalesLastYear]
-	,[person].[OrdersNum]
-FROM [dbo].[Person] [person]
-LEFT JOIN [Sales].[SalesPerson] [sales]
-	ON [person].[BusinessEntityID] = [sales].[BusinessEntityID]
-LEFT JOIN CTE_Calc_Sum cte
-	ON [person].[BusinessEntityID] = cte.[Bus];
+	 [BusinessEntityID]
+	 ,[PersonType]
+	 ,[NameStyle]
+	 ,[Title]
+	 ,[FirstName]
+	 ,[MiddleName]
+	 ,[LastName]
+	 ,[Suffix]
+	 ,[EmailPromotion]
+	 ,[ModifiedDate]
+	 ,[SalesYTD]
+	 ,[SalesLastYear]
+	 ,[OrdersNum]
+	 )
+SELECT prs.[BusinessEntityID]
+	 ,prs.[PersonType]
+	 ,prs.[NameStyle]
+	 ,prs.[Title]
+	 ,prs.[FirstName]
+	 ,prs.[MiddleName]
+	 ,prs.[LastName]
+	 ,prs.[Suffix]
+	 ,prs.[EmailPromotion]
+	 ,prs.[ModifiedDate]
+	 ,sls.[SalesYTD]
+	 ,sls.[SalesLastYear]
+	 ,cte.[OrdersNum]
+FROM [dbo].[Person] prs
+LEFT JOIN [Sales].[SalesPerson] sls
+	 ON prs.[BusinessEntityID] = sls.[BusinessEntityID]
+LEFT JOIN CteOrdersNum cte
+	 ON prs.[BusinessEntityID] = cte.[SalesPersonID];
+
+--SELECT *
+--FROM #Person
+--WHERE [PersonType] = N'SP';
 
 ------------------------------------------------------------------
 -- d) 
 -- удалите из таблицы dbo.Person одну строку
 -- (где BusinessEntityID = 290)
+DELETE
+FROM [dbo].[Person]
+WHERE [BusinessEntityID] = 290;
+
+
+--SELECT COUNT(*) FROM [dbo].[Person] WHERE [BusinessEntityID] = 290;
+
 ------------------------------------------------------------------
 -- e)
 -- напишите Merge выражение, использующее 
@@ -109,3 +120,54 @@ LEFT JOIN CTE_Calc_Sum cte
 -- Если в dbo.Person присутствует такая строка, 
 -- которой не существует во временной таблице, 
 -- удалите строку из dbo.Person.
+
+--SELECT COUNT(*) FROM #Person
+--SELECT COUNT(*) FROM [dbo].[Person]
+
+MERGE [dbo].[Person] AS trg
+USING #Person AS src
+	 ON (trg.[BusinessEntityID] = src.[BusinessEntityID])
+WHEN MATCHED
+	 THEN
+	 	 UPDATE
+	 	 SET trg.[SalesYTD] = src.[SalesYTD]
+	 	 	 ,trg.[SalesLastYear] = src.[SalesLastYear]
+	 	 	 ,trg.[OrdersNum] = src.[OrdersNum]
+WHEN NOT MATCHED BY TARGET
+	 THEN
+	 	 INSERT (
+	 	 	 [BusinessEntityID]
+	 	 	 ,[PersonType]
+	 	 	 ,[NameStyle]
+	 	 	 ,[Title]
+	 	 	 ,[FirstName]
+	 	 	 ,[MiddleName]
+	 	 	 ,[LastName]
+	 	 	 ,[Suffix]
+	 	 	 ,[EmailPromotion]
+	 	 	 ,[ModifiedDate]
+	 	 	 ,[SalesYTD]
+	 	 	 ,[SalesLastYear]
+	 	 	 ,[OrdersNum]
+	 	 	 )
+	 	 VALUES (
+	 	 	 src.[BusinessEntityID]
+	 	 	 ,src.[PersonType]
+	 	 	 ,src.[NameStyle]
+	 	 	 ,src.[Title]
+	 	 	 ,src.[FirstName]
+	 	 	 ,src.[MiddleName]
+	 	 	 ,src.[LastName]
+	 	 	 ,src.[Suffix]
+	 	 	 ,src.[EmailPromotion]
+	 	 	 ,src.[ModifiedDate]
+	 	 	 ,src.[SalesYTD]
+	 	 	 ,src.[SalesLastYear]
+	 	 	 ,src.[OrdersNum]
+	 	 	 )
+WHEN NOT MATCHED BY SOURCE
+	 THEN
+	 	 DELETE;
+
+--SELECT COUNT(*) FROM #Person
+--SELECT COUNT(*) FROM [dbo].[Person]
