76d6bda3b2aa068f9b5aad466b5ceda3c368f45a
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-10-12 01:23:22 +0300

    4-2 in progress

--- lab4/lab4task2.sql
+++ lab4/lab4task2.sql
@@ -12,12 +12,8 @@ WITH ENCRYPTION, SCHEMABINDING AS
 SELECT 
     ct.[ProductCategoryID] AS [ProductCategoryID]
 	, ct.[Name] AS [CategoryName]
-	, ct.[rowguid] AS [CategoryRowGuid]
-	, ct.[ModifiedDate] AS [CategoryModifiedDate]
 	, subct.[ProductSubcategoryID] AS [ProductSubcategoryID]
 	, subct.[Name] AS [SubcategoryName]
-	, subct.[rowguid] AS [SubcategoryRowGuid]
-	, subct.[ModifiedDate] AS [SubcategoryModifiedDate]
 FROM [Production].[ProductCategory] AS ct
 JOIN [Production].[ProductSubcategory] AS subct
     ON (ct.[ProductCategoryID] = subct.[ProductSubcategoryID])
@@ -30,7 +26,7 @@ GO
 
 -- b) Создайте три INSTEAD OF триггера для представления 
 -- на операции INSERT, UPDATE, DELETE. Каждый триггер 
--- должен выполнять соответствующие операции в таблицах 
+-- должен выполнять СООТВЕТСТВУЮЩИЕ операции в таблицах 
 -- Production.ProductCategory и Production.ProductSubcategory.
 DROP TRIGGER IF EXISTS [Production].[TRG_vCategoryUpdate];
 GO
@@ -41,28 +37,22 @@ INSTEAD OF UPDATE
 AS
 IF EXISTS(SELECT 1 FROM INSERTED) AND EXISTS (SELECT 1 FROM DELETED)
 BEGIN
+	PRINT 'UPDATE CALLED'
 	DECLARE @date DATETIME = GETDATE();
 
     UPDATE [Production].[ProductSubcategory]
     SET 
     [Name] = ins.[SubcategoryName],
-    [rowguid] = ins.[SubcategoryRowGuid],
     ModifiedDate = @date
     FROM INSERTED ins
 	
-	--DECLARE @insCategoryName Name = ins.[CategoryName];
-
-	--IF NOT EXISTS (SELECT 1 FROM [Production].[ProductCategory] WHERE )
-	--BEGIN
-	--	UPDATE [Production].[ProductCategory]
-	--	SET 
-	--	[Name] = ins.[CategoryName],
-	--	[rowguid] = ins.[CategoryRowGuid],
-	--	ModifiedDate = @date
-	--	FROM INSERTED ins
-	--END
+	UPDATE [Production].[ProductCategory]
+    SET 
+    [Name] = ins.[CategoryName],
+    ModifiedDate = @date
+    FROM INSERTED ins
 END
-
+ 
 DROP TRIGGER IF EXISTS [Production].[TRG_vCategoryDelete];
 GO
 
@@ -72,11 +62,11 @@ INSTEAD OF DELETE
 AS
 IF NOT EXISTS(SELECT 1 FROM INSERTED) AND EXISTS (SELECT 1 FROM DELETED)
 BEGIN
-	DELETE FROM [Production].[ProductCategory]
-	WHERE [ProductCategoryID] IN (SELECT [ProductCategoryID] FROM DELETED)
-
     DELETE FROM [Production].[ProductSubcategory]
 	WHERE [ProductSubcategoryID] IN (SELECT [ProductSubcategoryID] FROM DELETED)
+
+	DELETE FROM [Production].[ProductCategory]
+	WHERE [ProductCategoryID] IN (SELECT [ProductCategoryID] FROM DELETED)
 END
 
 DROP TRIGGER IF EXISTS [Production].[TRG_vCategoryInsert];
@@ -91,21 +81,51 @@ BEGIN
 	DECLARE @date DATETIME = GETDATE();
 
 	INSERT INTO [Production].[ProductCategory] ([Name], [ModifiedDate], [rowguid])
-    SELECT Name, ReasonType, GETDATE()
-    FROM inserted
+    SELECT [CategoryName], @date, NEWID()
+    FROM INSERTED
     
-    INSERT INTO Sales.SalesOrderHeaderSalesReason (SalesOrderId, SalesReasonID, ModifiedDate)
-    SELECT I.SalesOrderID, I.SalesReasonID, GETDATE()
-    FROM inserted I;
+    INSERT INTO [Production].[ProductSubcategory] ([ProductCategoryID], [Name], [ModifiedDate], [rowguid])
+    SELECT @@IDENTITY, [SubcategoryName], @date, NEWID()
+    FROM INSERTED;
 END
+GO
 
-
-    
-
+   
 -- c) Вставьте новую строку в представление, 
 -- указав новые данные для ProductCategory и ProductSubcategory.
 -- Триггер должен добавить новые строки в таблицы 
 -- Production.ProductCategory и Production.ProductSubcategory. 
 -- Обновите вставленные строки через представление. 
 -- Удалите строки.
+
+DECLARE @cat NVARCHAR(50) = CONVERT(NVARCHAR(50), SYSDATETIME());
+DECLARE @subc NVARCHAR(50) = CONVERT(NVARCHAR(50), SYSDATETIME());
+
+INSERT INTO [Production].[vCategory]( 
+    [CategoryName]
+,	[SubcategoryName]) 
+VALUES
+(
+	@cat,
+	@subc
+)
+
+SELECT * FROM [Production].[ProductCategory] 
+SELECT * FROM [Production].[ProductSubcategory]
+
+UPDATE [Production].[vCategory]
+SET [CategoryName] = N'1',
+	[SubcategoryName] = N'2'
+WHERE [CategoryName] = @cat AND [SubcategoryName] = @subc
+
+
+SELECT * FROM [Production].[ProductCategory] 
+SELECT * FROM [Production].[ProductSubcategory]
+
+DELETE FROM [Production].[ProductSubcategory]  WHERE [ProductSubcategoryID] > 37 
+DELETE FROM [Production].[ProductCategory]  WHERE [ProductCategoryID] > 4 
+
+SELECT * FROM [Production].[ProductCategory] 
+SELECT * FROM [Production].[ProductSubcategory]
+
 
