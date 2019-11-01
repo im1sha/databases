2952d50717fe31cb2496f5a2f1a002ce2d093999
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-11-01 23:34:53 +0300

    Implement lab6

--- lab6/lab6.sql
+++ lab6/lab6.sql
@@ -6,3 +6,32 @@
 -- 
 -- Список лет передайте в процедуру через входной параметр.
 
+DROP PROCEDURE
+IF EXISTS [dbo].[pOrdersByYearList]
+GO
+
+CREATE PROCEDURE [dbo].[pOrdersByYearList] (@Years NVARCHAR(100))
+AS
+BEGIN
+	 DECLARE @query AS NVARCHAR(500)
+
+	 SET @query =  'SELECT Name
+							,' + @Years + '
+					FROM (
+							SELECT [Name]
+	 							,YEAR([OrderDate]) AS y
+	 							,[OrderQty]
+							FROM [Sales].[SalesOrderHeader]
+							JOIN [Sales].[SalesOrderDetail]
+	 							ON [SalesOrderDetail].[SalesOrderID] = [SalesOrderHeader].[SalesOrderID]
+							JOIN [Production].[Product]
+	 							ON [Product].[ProductID] = [SalesOrderDetail].[ProductID]
+							) AS selected
+					PIVOT(SUM(OrderQty) FOR y IN (' + @Years + ')) AS pvt'
+
+	 EXEC (@query)
+END
+GO
+
+EXECUTE [dbo].[pOrdersByYearList] '"2005", "2006"'
+GO
