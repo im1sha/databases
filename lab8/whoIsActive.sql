ab140a8dbe53603eade802e6774a7aef629373d5
im1sha <mikefoxx2000@ya.ru>
2019-11-06 18:13:55 +0300

    Extend whoIsActive

--- lab8/whoIsActive.sql
+++ lab8/whoIsActive.sql
@@ -5,6 +5,14 @@ SELECT sqltext.[text]
 	,req.[command]
 	,req.[cpu_time]
 	,req.[total_elapsed_time]
+	,DB_NAME(req.[database_id]) AS DatabaseName
+	,req.[blocking_session_id]
+	,req.[wait_type]
+	,req.[wait_time]
+	,req.[open_transaction_count]
+	,req.[estimated_completion_time]
+	,req.[reads]
+	,req.[writes]
 FROM [sys].[dm_exec_requests] req
 CROSS APPLY [sys].[dm_exec_sql_text]([sql_handle]) AS sqltext
 
